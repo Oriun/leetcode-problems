9d21407f264d08b12bc3a205b572a9de4ea4b0bb
Oriun <71255433+Oriun@users.noreply.github.com>
2022-02-12 23:12:05 +0100

    day 9: 3/3 medium

---
+++ 31 - 40/0033. Search In Rotated Sorted Array/index.1.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+    if(nums.length === 1) return nums == target ? 0 : -1
+    let j = 0, k = nums.length - 1, l
+    if(nums[j] === target) return j
+    if(nums[k] === target) return k
+    let rotated = nums[j] > nums[k]
+    let inLeftPart = target > nums[k]
+    
+    let i = 10
+    while(k-j-1 && i){
+        l = Math.floor((j+k) * .5)
+        if(nums[l] === target) return l
+        if(rotated){
+            if(!inLeftPart){
+                if(!(nums[l] < target) && nums[l] < nums[nums.length - 1])
+                    k = l
+                else j = l
+            }else{
+                if(nums[l] > nums[0] && nums[l] < target) j = l
+                else k = l
+            }
+        }else{
+            if(nums[l] < target) j = l
+            if(nums[l] > target) k = l
+        }
+    }
+    return nums[k] === target ? k : nums[j] === target ? j : -1
+};
