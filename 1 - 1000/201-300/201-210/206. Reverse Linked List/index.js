67129858ea4eeedb4482cf35bc27be0dee4c71d0
Oriun <emmanuel.nuiro@epitech.eu>
2022-08-11 23:12:05 +0200

    LeetCode 75 Day #3 Ended
    Signed-off-by: Oriun <emmanuel.nuiro@epitech.eu>

---
+++ 1 - 1000/201-300/201-210/206. Reverse Linked List/index.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function (head) {
+  let previous = null;
+  let current = head;
+  let next = head?.next;
+  while (current) {
+    current.next = previous;
+    previous = current;
+    current = next;
+    next = next?.next;
+  }
+  return previous;
+};
